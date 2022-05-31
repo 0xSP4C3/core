85495c5eb6a021962148f9a3b1a9615516ba021f
Benjamin 'PenguinJoints' Wang <p3nj@tutanota.com>
2022-05-31 17:57:54 +0800

    Add exchange credentials

---
+++ pkg/repository/exchange_credentials.go
@@ -0,0 +1,7 @@
+package repository
+
+const (
+    ExchangeCreateCredential    string  =   "exchange:create"
+    ExchangeUpdateCredential    string  =   "exchange:update"
+    ExchangeDeleteCredential    string  =   "exchange:delete"
+)
