37c9fd461d413ba839050e2bebb9b1c2213bc747
Benjamin 'PenguinJoints' Wang <p3nj@tutanota.com>
2022-05-31 16:22:14 +0800

    add Get Exchange by Id endpoint

--- app/controllers/exchange_controller.go
+++ app/controllers/exchange_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/0xsp4c3/core/platform/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 // GetExchange funcs gets all exists exchanges.
@@ -34,5 +35,47 @@ func GetExchanges(c *fiber.Ctx) error {
         "msg":      nil,
         "count":    len(exchanges),
         "exchanges":exchanges,
+    })
+}
+
+// GetExchange func gets exchange by given ID or 404 error.
+// @Description Get exchange by given ID.
+// @Summary get exchange by given ID
+// @Tags Exchange
+// @Accept json
+// @Produce json
+// @Param id path string true "Exchange ID"
+// @Success 200 {object} models.exchange
+// @Router /v1/exchange/{id} [get]
+func GetExchange(c *fiber.Ctx) error {
+    id, err := uuid.Parse(c.Params("id"))
+    if err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    db, err := database.OpenDBConnection()
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    exchange, err := db.GetExchange(id)
+    if err != nil {
+        return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+            "error":    true,
+            "msg":      "exchange with the given ID is not found",
+            "exchange": nil,
+        })
+    }
+
+    return c.JSON(fiber.Map{
+        "error":    false,
+        "msg":      nil,
+        "exchange": exchange,
     })
 }
--- app/queries/exchange_query.go
+++ app/queries/exchange_query.go
@@ -2,6 +2,7 @@ package queries
 
 import (
 	"github.com/0xsp4c3/core/app/models"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -11,11 +12,24 @@ type ExchangeQueries struct {
 
 
 func (q *ExchangeQueries) GetExchanges() ([]models.Exchange, error) {
-    exchange := []models.Exchange{}
+    exchanges := []models.Exchange{}
 
     query := `SELECT * FROM exchanges`
 
-    err := q.Get(&exchange, query)
+    err := q.Get(&exchanges, query)
+    if err != nil {
+        return exchanges, err
+    }
+
+    return exchanges, nil
+}
+
+func (q *ExchangeQueries) GetExchange(id uuid.UUID) (models.Exchange, error) {
+    exchange := models.Exchange{}
+
+    query := `SELECT * FROM exchanges WHERE ID = $1`
+
+    err := q.Get(&exchange, query, id)
     if err != nil {
         return exchange, err
     }
