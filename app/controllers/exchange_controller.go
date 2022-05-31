0e5bc871e44137d3ec2dd2807b6f9ca556e3115e
Benjamin 'PenguinJoints' Wang <p3nj@tutanota.com>
2022-05-31 17:58:27 +0800

    add Create Exchange endpoint

--- app/controllers/exchange_controller.go
+++ app/controllers/exchange_controller.go
@@ -1,6 +1,11 @@
 package controllers
 
 import (
+	"time"
+
+	"github.com/0xsp4c3/core/app/models"
+	"github.com/0xsp4c3/core/pkg/repository"
+	"github.com/0xsp4c3/core/pkg/utils"
 	"github.com/0xsp4c3/core/platform/database"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
@@ -76,6 +81,82 @@ func GetExchange(c *fiber.Ctx) error {
     return c.JSON(fiber.Map{
         "error":    false,
         "msg":      nil,
+        "exchange": exchange,
+    })
+}
+
+func CreateExchange(c *fiber.Ctx) error {
+    currentTime := time.Now().Unix()
+
+    claims, err := utils.ExtractTokenMetadata(c)
+    if err != nil {
+        c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    expireTime := claims.Expires
+
+    if currentTime > expireTime {
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+            "error":    true,
+            "msg":      "Unauthorized. Token expired.",
+        })
+    }
+
+    credential := claims.Credentials[repository.ExchangeCreateCredential]
+    if !credential {
+        return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+            "error":    true,
+            "msg":      "Forbidden. Permission denied.",
+        })
+    }
+    
+    exchange := &models.Exchange{}
+
+    if err := c.BodyParser(exchange); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    db, err := database.OpenDBConnection()
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    time := time.Now()
+
+    validate := utils.NewValidator()
+
+    exchange.ID = uuid.New()
+    exchange.CreatedAt = time
+    exchange.UpdatedAt = time
+    exchange.IsBlocked = false
+    exchange.IsEnabled = true
+
+    if err := validate.Struct(exchange); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error":    true,
+            "msg":      utils.ValidatorErrors(err),
+        })
+    }
+
+    if err := db.CreateExchange(exchange); err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error":    true,
+            "msg":      err.Error(),
+        })
+    }
+
+    return c.Status(fiber.StatusOK).JSON(fiber.Map{
+        "error":    false,
+        "msg":      "Exchange created!",
         "exchange": exchange,
     })
 }
