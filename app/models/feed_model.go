2d86c8e884bc3bf66d230a01df9fb68c0fb3247c
Benjamin 'PenguinJoints' Wang <p3nj@tutanota.com>
2022-06-11 10:40:34 +0800

    Add feed model

--- app/models/feed_model.go
+++ app/models/feed_model.go
@@ -1,1 +1,40 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Feed struct {
+    ID          uuid.UUID
+    CreatedAt   time.Time
+    UpdatedAt   time.Time
+    OpenBid     int64
+    CloseBid    int64
+    HighestBid  int64
+    LowestBid   int64
+    TotalTrade  int64
+    BaseVolume  int64
+    QuoteVolume int64
+    CoinID      uuid.UUID
+    FeedTimeID  uuid.UUID
+    FeedRangeID uuid.UUID
+}
+
+type FeedTime struct {
+    ID          uuid.UUID
+    CreatedAt   time.Time
+    UpdatedAt   time.Time
+    StartAt     time.Time
+    EndedAt     time.Time
+}
+
+type FeedRange struct {
+    ID          uuid.UUID
+    CreatedAt   time.Time
+    UpdatedAt   time.Time
+    Name        string
+    Description string
+    IsEnabled   bool
+}
